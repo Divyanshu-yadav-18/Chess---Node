473516e7575e61b70e99ce3235dc447d9f5bdf9b
Divyanshu-yadav-18 <rishabhyadav14065@gmail.com>
2025-06-21 18:31:32 +0530

    react-router-dom

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,17 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router";
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1 className="text-4xl bg-sky-50 font-bold text-blue-600">Tailwind works!</h1>
-      <button className='bg-red-800'>
-        Join Room
-      </button>
+      <BrowserRouter basename='/app'>
+        <Routes>
+          <Route path='/'/>{
+            
+          }
+
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
