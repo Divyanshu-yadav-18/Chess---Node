60ee8e9940c392ac32e0312fd0c3794cf40cde02
Divyanshu-yadav-18 <rishabhyadav14065@gmail.com>
2025-06-22 22:44:55 +0530

    feat: landing page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,21 +1,22 @@
-import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Route, Routes } from "react-router";
+import { Game } from './Screens/game';
+import { Landing } from './Screens/landing';
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <BrowserRouter basename='/app'>
-        <Routes>
-          <Route path='/'/>{
-            
-          }
+    <div className='h-screen bg-slate-800'>
 
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing/>}/>
+          <Route path="/game" element={<Game/>}/>
         </Routes>
       </BrowserRouter>
+    </div>
     </>
   )
 }
---
+++ Frontend/src/Screens/game.tsx
@@ -0,0 +1,5 @@
+export const Game = ()=>{
+    return <div>
+        <h1>Game</h1>
+    </div>
+}
