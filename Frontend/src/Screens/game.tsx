931b85e7dbf58abaded3f082a877a078a2c9b7f7
Divyanshu-yadav-18 <rishabhyadav14065@gmail.com>
2025-06-23 18:45:06 +0530

    game page

--- Frontend/src/Screens/game.tsx
+++ Frontend/src/Screens/game.tsx
@@ -1,5 +1,14 @@
 export const Game = ()=>{
-    return <div>
-        <h1>Game</h1>
+    return <div className="justify-center flex">
+        <div className="pt-8 max-w-screen-lg w-full">
+            <div className="grid grid-cols-6 gap-4 w-full" >
+                <div className="col-span-4 bg-red-200 w-full">
+                    <ChessBoard/>
+                </div>
+                <div className="col-span-2 bg-green-200 w-full">
+                    <button>Play</button>
+                </div>
+            </div>
+        </div>
     </div>
 }
