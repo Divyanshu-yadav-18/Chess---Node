ebe38fece0e229b493aebafeb57b825a5af67fe4
Divyanshu-yadav-18 <rishabhyadav14065@gmail.com>
2025-06-25 22:40:04 +0530

    board

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-    <div className='h-screen bg-slate-800'>
+    <div className='h-screen w-screen bg-slate-800'>
 
       <BrowserRouter>
         <Routes>
--- Frontend/src/components/chessboard.tsx
+++ Frontend/src/components/chessboard.tsx
@@ -1,7 +1,50 @@
-export const ChessBoard=()=>{
-    return (
-        <>
-            <h1>Hello</h1>
-        </>
-    )
+import type { Color, PieceSymbol, Square } from "chess.js";
+import { useState } from "react";
+import { MOVE } from "../Screens/game";
+
+export const ChessBoard=({ board, socket }:{
+    board: ({
+        square: Square;
+        type: PieceSymbol;
+        color : Color;
+    } | null)[][];
+    socket: WebSocket;
+})=>{
+
+    const [from, setFrom] = useState<null | Square>(null);
+    const [to, setTo] = useState<null | Square>(null);
+
+
+    return <div className="text-white-200">
+        {board.map((row,i) =>{
+            return <div key={i} className="flex">
+                {row.map((square, j)=>{
+                    const squareRepresentation = String.fromCharCode(65 +(j % 8)) +""+ (8-i) as Square;
+
+                    return <div onClick={()=>{
+                        if(!from){
+                            setFrom(squareRepresentation);
+                        }else{
+                            setTo(square?.square ?? null);
+                            socket.send(JSON.stringify({
+                                type:MOVE,
+                                payload: {
+                                    from,
+                                    to: squareRepresentation
+                                }
+                            }))
+                            setFrom(null)
+                        }
+                    }}
+                     key={j} className={`w-16 h-16 ${(i+j)%2===0 ? 'bg-green-500': 'bg-green-300'}`}>
+                        <div className="w-full justify-center flex h-full">
+                            <div className="h-full justify-center flex flex-col">
+                                {square ? square.type : ""}
+                            </div>
+                        </div>
+                    </div>
+                })}
+            </div>
+        })}
+    </div>
 }
